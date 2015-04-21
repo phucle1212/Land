8ddde43c76e348f77bb27d40ecd145aea57c0d52
phucle1212 <phucle1212@gmail.com>
2015-04-21 08:11:32 +0700

    change js file

--- application/views/layout/frontend.php
+++ application/views/layout/frontend.php
@@ -10,8 +10,6 @@
 
     <!-- JS here -->
     <script type="text/javascript" src="<?php echo base_url(); ?>public/template/frontend/js/jquery-1.11.2.min.js"></script>
-    // <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-    // <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url(); ?>public/template/frontend/js/bootstrap.js"></script>
     <script type="text/javascript" src="<?php echo base_url(); ?>public/template/frontend/js/bootstrap.min.js"></script>
 
